f06e21aba59fe495f76d5ba67f62da1c09d4b27a
Oliver Zott <zott_oliver@web.de>
2021-10-10 14:24:45 +0200

    Controller - Unit of work now adding/update DateTime on Save(). ToDo: update method not working correctly; type-defaults are set for fields.

--- CarRentalManagement/Server/Repository/IUnitOfWork.cs
+++ CarRentalManagement/Server/Repository/IUnitOfWork.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CarRentalManagement.Shared.Domain;
+using Microsoft.AspNetCore.Http;
 
 namespace CarRentalManagement.Server.Repository
 {
     public interface IUnitOfWork : IDisposable
     {
         IGenericRepository<Make> Make { get; }
+        IGenericRepository<Color> Color { get; }
         //IGenericRepository<Model> Model { get; }
         //IGenericRepository<Vehicle> Vehicle { get; }
-        //IGenericRepository<Color> Color { get; }
         //IGenericRepository<Customer> Customer { get; }
         //IGenericRepository<Booking> Booking { get; }
 
-        Task Save();
+        Task Save(HttpContext httpContext);
     }
 }
--- CarRentalManagement/Server/Repository/UnitOfWork.cs
+++ CarRentalManagement/Server/Repository/UnitOfWork.cs
@@ -1,6 +1,10 @@
 ﻿using CarRentalManagement.Server.Data;
+using CarRentalManagement.Server.Data.Migrations;
 using CarRentalManagement.Shared.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace CarRentalManagement.Server.Repository
@@ -9,6 +13,7 @@ namespace CarRentalManagement.Server.Repository
     {
         private readonly ApplicationDbContext _context;
         private IGenericRepository<Make> _make;
+        private IGenericRepository<Color> _color;
 
         public UnitOfWork(ApplicationDbContext context)
         {
@@ -16,6 +21,7 @@ namespace CarRentalManagement.Server.Repository
         }
 
         public IGenericRepository<Make> Make => _make ?? new GenericRepository<Make>(_context);
+        public IGenericRepository<Color> Color => _color ?? new GenericRepository<Color>(_context);
 
         public void Dispose()
         {
@@ -23,8 +29,26 @@ namespace CarRentalManagement.Server.Repository
             GC.SuppressFinalize(this);
         }
 
-        public async Task Save()
+        public async Task Save(HttpContext httpContext)
         {
+            var user = httpContext.User.Identity.Name;
+
+
+            // get updated/created entities to update DateTime
+            var entries = _context.ChangeTracker.Entries()
+                .Where(q => q.State is EntityState.Modified or EntityState.Added);
+
+            foreach (var entityEntry in entries)
+            {
+                ((BaseDomainModel)entityEntry.Entity).DateUpdated = DateTime.Now;
+                ((BaseDomainModel)entityEntry.Entity).UpdatedBy = user;
+                if (entityEntry.State == EntityState.Added)
+                {
+                    ((BaseDomainModel)entityEntry.Entity).DateCreated = DateTime.Now;
+                    ((BaseDomainModel)entityEntry.Entity).CreatedBy = user;
+                }
+            }
+
             await _context.SaveChangesAsync();
         }
     }
