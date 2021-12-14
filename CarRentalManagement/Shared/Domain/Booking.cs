5f5edb13e98f17d409829f32f492c59ec2dd91a6
Oliver Zott <oliver.zott@vertigis.com>
2021-12-14 11:23:41 +0100

    Date validation and minor adaptions.

--- CarRentalManagement/Shared/Domain/Booking.cs
+++ CarRentalManagement/Shared/Domain/Booking.cs
@@ -1,14 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CarRentalManagement.Shared.Domain
 {
-    public class Booking : BaseDomainModel
+    public class Booking : BaseDomainModel, IValidatableObject
     {
-        public DateTime DateIn { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime DateOut { get; set; }
+        public DateTime? DateIn { get; set; }
         public int CustomerId { get; set; }
         public virtual Customer Customer { get; set; }
         public int VehicleId { get; set; }
         public virtual Vehicle Vehicle { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (DateIn == null) yield break;
+            if (DateIn <= DateOut)
+            {
+                yield return new ValidationResult("Date-In must be greater then Date-Out!", new[] { "DateIn"});
+            }
+        }
+
     }
 }
